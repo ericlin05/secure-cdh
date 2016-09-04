a6ea100e99cd111d2766adaea45733954b4d7a9c
Eric Lin <ericlin@ericlin.me>
2016-09-04 20:49:24 +1000

    Refactored API call to each classes

--- CMAPI.py
+++ CMAPI.py
@@ -4,63 +4,36 @@ from StringIO import StringIO
 
 class CMAPI:
     def __init__(self, host, username, password):
+        self.SERVICE_HIVE = "Hive"
+        self.SERVICE_IMPALA = "Impala"
+        self.SERVICE_SOLR = "Solr"
+        self.SERVICE_HUE = "Hue"
+        self.SERVICES = [self.SERVICE_HIVE, self.SERVICE_IMPALA, self.SERVICE_SOLR, self.SERVICE_HUE]
+
         self.host = host
         self.username = username
         self.password = password
+        self.service_name = None
+        self.api_url = None
+        self.cluster_name = None
+        self.service_names = {}
+        self.service_type = None
+        self.init()
+
+    def init(self):
         self.api_url = "{0}/api/{1}".format(
             self.host,
             self.get_version()
         )
 
         self.cluster_name = self.get_cluster_name()
-
-        self.hive_service_name = None
-        self.yarn_service_name = None
-        self.impala_service_name = None
-        self.hue_service_name = None
-        self.solr_service_name = None
-
-        self.hive_config_url = None
-        self.yarn_config_url = None
-        self.impala_config_url = None
-        self.hue_config_url = None
-        self.solr_config_url = None
-
         self.get_service_names()
 
-        if self.hive_service_name is not None:
-            self.hive_config_url = "{0}/clusters/{1}/services/{2}/config".format(
+    def get_config_url(self, service_name):
+        return "{0}/clusters/{1}/services/{2}/config".format(
                 self.api_url,
                 self.cluster_name,
-                self.hive_service_name
-            )
-
-        if self.yarn_service_name is not None:
-            self.yarn_config_url = "{0}/clusters/{1}/services/{2}/config".format(
-                self.api_url,
-                self.cluster_name,
-                self.yarn_service_name
-            )
-
-        if self.impala_service_name is not None:
-            self.impala_config_url = "{0}/clusters/{1}/services/{2}/config".format(
-                self.api_url,
-                self.cluster_name,
-                self.impala_service_name
-            )
-
-        if self.hue_service_name is not None:
-            self.hue_config_url = "{0}/clusters/{1}/services/{2}/config".format(
-                self.api_url,
-                self.cluster_name,
-                self.hue_service_name
-            )
-
-        if self.solr_service_name is not None:
-            self.solr_config_url = "{0}/clusters/{1}/services/{2}/config".format(
-                self.api_url,
-                self.cluster_name,
-                self.solr_service_name
+                service_name
             )
 
     def curl_get(self, url):
@@ -108,51 +81,22 @@ class CMAPI:
         return data["items"][0]["name"]
 
     def get_service_names(self):
-        url = "{0}/clusters/{1}/services".format(self.api_url, self.cluster_name)
-        data = json.loads(self.curl_get(url))
+        data = json.loads(self.get_services())
         for d in data["items"]:
-            if d["type"] == "HIVE":
-                self.hive_service_name = d["name"]
+            self.service_names[d["type"]] = d["name"]
 
-            if d["type"] == "YARN":
-                self.yarn_service_name = d["name"]
-
-            if d["type"] == "IMPALA":
-                self.impala_service_name = d["name"]
-
-            if d["type"] == "HUE":
-                self.hue_service_name = d["name"]
-
-            if d["type"] == "SOLR":
-                self.solr_service_name = d["name"]
-
-    def get_hive_configs(self):
-        return self.curl_get(self.hive_config_url + "?view=full")
-
-    def get_yarn_configs(self):
-        return self.curl_get(self.yarn_config_url + "?view=full")
-
-    def get_impala_configs(self):
-        return self.curl_get(self.impala_config_url + "?view=full")
+    def get_service_name(self, type):
+        if self.service_names[type] is not None:
+            return self.service_names[type]
 
     def enable_sentry(self):
-        if self.hive_config_url is not None:
-            data = {"items" : [ { "name" : "sentry_service", "value" : "sentry" } ] }
-            self.curl_put(self.hive_config_url, json.dumps(data))
+        for service in self.SERVICES:
+            if service.upper() in self.service_names:
+                className = service + "CMAPIClient"
 
-        if self.yarn_config_url is not None:
-            data = {"items": [{"name": "min_user_id", "value": "1000"}]}
-            self.curl_put(self.yarn_config_url, json.dumps(data))
+                module = __import__(className)
+                class_ = getattr(module, className)
+                client = class_(self)
 
-        if self.impala_config_url is not None:
-            data = {"items": [{"name": "sentry_service", "value": "sentry"}]}
-            self.curl_put(self.impala_config_url, json.dumps(data))
-
-        if self.hue_config_url is not None:
-            data = {"items": [{"name": "sentry_service", "value": "sentry"}]}
-            self.curl_put(self.hue_config_url, json.dumps(data))
-
-        if self.solr_config_url is not None:
-            data = {"items": [{"name": "sentry_service", "value": "sentry"}]}
-            self.curl_put(self.solr_config_url, json.dumps(data))
+                client.enable_sentry()
 
---
+++ ImpalaCMAPIClient.py
@@ -0,0 +1,12 @@
+import json
+
+class ImpalaCMAPIClient():
+    def __init__(self, api):
+        self.api = api
+        self.config_url = api.get_config_url(
+            api.get_service_name(api.SERVICE_IMPALA.upper())
+        )
+
+    def enable_sentry(self):
+        data = {"items" : [ { "name" : "sentry_service", "value" : "sentry" } ] }
+        self.api.curl_put(self.config_url, json.dumps(data))
