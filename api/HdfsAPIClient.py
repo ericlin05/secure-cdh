d26430bb76cbb25fcbd3e12bcfa529e360b7284e
Eric Lin <ericlin@ericlin.me>
2016-09-06 16:06:30 +1000

    update Hdfs api to get correct role groups

--- api/HdfsAPIClient.py
+++ api/HdfsAPIClient.py
@@ -10,8 +10,12 @@ class HdfsAPIClient:
         })
 
         # update datanode configurations
-        self.service.get_role_config_group('HDFS-DATANODE-BASE').update_config({
-                'dfs_datanode_port': "1004",
-                "dfs_datanode_http_port": "1006",
-                "dfs_datanode_data_dir_perm": "700"
-            })
+        role_groups = self.service.get_all_role_config_groups()
+        for role_group in role_groups:
+            if str(role_group.name).find('DATANODE') > 0:
+                role_group.update_config({
+                    'dfs_datanode_port': "1004",
+                    "dfs_datanode_http_port": "1006",
+                    "dfs_datanode_data_dir_perm": "700"
+                })
+                break
